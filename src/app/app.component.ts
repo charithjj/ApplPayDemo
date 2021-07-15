fd797f8e36242d99134b90cd9efdd81489dda8b2
Charith Jayasundara <charith@jayasundara.net>
2021-07-15 10:41:23 +1000

    ApplePayAPI test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,49 +32,59 @@ export class AppComponent {
     }
     else
     {
-      console.log("apple session not found. can not make payments");
-      this.messageInfo = "apple session not found. can not make payments";
+      console.log("apple session not found for ApplePayJS.");
+      this.messageInfo = "apple session not found for ApplePayJS.";
     }
-    if ((winRef.nativeWindow as any).ApplePaySession) {
+    // if ((winRef.nativeWindow as any).ApplePaySession) {
 
-      console.log("apple session found");
-      this.messageInfo = "apple session found";
-      // var merchantIdentifier = 'example.com.store';
-      // var promise = winRef.nativeWindow.ApplePaySession.canMakePaymentsWithActiveCard(merchantIdentifier);
-    }
-    else
-    {
-      console.log("apple session not found");
-    }
+    //   console.log("apple session found");
+    //   this.messageInfo = "apple session found";
+    //   // var merchantIdentifier = 'example.com.store';
+    //   // var promise = winRef.nativeWindow.ApplePaySession.canMakePaymentsWithActiveCard(merchantIdentifier);
+    // }
+    // else
+    // {
+    //   console.log("apple session not found");
+    // }
+
+    this.ApplePayApiTest();
   }
   
   public ApplePayApiTest() {
-    if (!(this._window as any).ApplePaySession) {
+    if (!(this._window as any).ApplePaySession) 
+    {
+      console.log("apple session not found for ApplePayAPI.");
+      this.messageInfo = this.messageInfo + "    : apple session not found for ApplePayAPI.";
       return;
-  }
+    }
+    else
+    {
+      console.log("apple session available for ApplePayAPI.");
+      this.messageInfo = this.messageInfo + "   :apple session available for ApplePayAPI.";
+    }
   
-  // Define ApplePayPaymentRequest
-  const request = {
-      "countryCode": "US",
-      "currencyCode": "USD",
-      "merchantCapabilities": [
-          "supports3DS"
-      ],
-      "supportedNetworks": [
-          "visa",
-          "masterCard",
-          "amex",
-          "discover"
-      ],
-      "total": {
-          "label": "Demo (Card is not charged)",
-          "type": "final",
-          "amount": "1.99"
-      }
-  };
-  
-  // Create ApplePaySession
-  const session = new (this._window as any).ApplePaySession(3, request);
+    // Define ApplePayPaymentRequest
+    const request = {
+        "countryCode": "US",
+        "currencyCode": "USD",
+        "merchantCapabilities": [
+            "supports3DS"
+        ],
+        "supportedNetworks": [
+            "visa",
+            "masterCard",
+            "amex",
+            "discover"
+        ],
+        "total": {
+            "label": "Demo (Card is not charged)",
+            "type": "final",
+            "amount": "1.99"
+        }
+    };
+    
+    // Create ApplePaySession
+    const session = new (this._window as any).ApplePaySession(3, request);
 
     
   }
